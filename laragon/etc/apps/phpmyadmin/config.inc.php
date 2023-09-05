511737ac206b1601083e3c289d9239d29d14998c
velome <gorez@net-c.be>
2023-09-05 14:26:50 +0200

    Update config.inc.php

--- laragon/etc/apps/phpmyadmin/config.inc.php
+++ laragon/etc/apps/phpmyadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
 
 /**
